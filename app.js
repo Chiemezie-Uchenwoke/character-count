3aa2dd08d7011a27062cd22005cce4dc85802624
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2024-11-07 02:45:42 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const textarea = document.getElementById('word-count');
+let updateCharacterCount = document.getElementById("update-char");
+const maximumChar = 100;
+
+textarea.addEventListener("input", () => {
+    let numberOfCharacters = textarea.value.length;    
+
+    if (numberOfCharacters > 100){
+        updateCharacterCount.textContent = "You have exceeded by " + (maximumChar - numberOfCharacters);
+        updateCharacterCount.style.color = "#ff2424";
+    } 
+
+    else if (numberOfCharacters === 200){
+        textarea.value = textarea.value.slice(201, textarea.value.length);
+    }
+    
+    else {
+        updateCharacterCount.textContent = (maximumChar - numberOfCharacters) + " Characters left";
+        updateCharacterCount.style.color = "#1fb7f3";
+    }
+    
+
+  textarea.style.height = "6rem";  // Reset the height
+//   textarea.style.height = textarea.scrollHeight + "px"; 
+
+});
+
+// Close and Post Events
+const close = document.getElementById("close");
+const post = document.getElementById("post");
+const postElement = document.getElementById("parent");
+
+close.addEventListener("click", () => {
+    postElement.style.display = "none";
+    post.style.display = "block";
+    textarea.value = "";
+});
+
+post.addEventListener("click", () => {
+    let numberOfCharacters = textarea.value.length; 
+
+    post.style.display = "none";
+    postElement.style.display = "block";
+    updateCharacterCount.textContent = (maximumChar - numberOfCharacters) + " Characters left";
+    updateCharacterCount.style.color = "#1fb7f3";
+});
